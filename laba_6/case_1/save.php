2088934ba55b69842a6cf427217d64e79a8e9080
pavel.karachev <pavel.karachev@team.wrike.com>
2018-12-16 23:33:01 +0300

    Добавляет 3 кейс для 6 лабы

--- laba_6/case_1/save.php
+++ laba_6/case_1/save.php
@@ -4,7 +4,7 @@ $name = $_POST['login'];
 $pass = $_POST['password'];
 $text = "Логин: $name \n"; 
 $tex2 = "Пароль: $pass \n";
-fputs($file,$text);
+fputs($file,$text); 
 fputs($file,$text2);
 fclose($file);
 header("Location:index.html");
